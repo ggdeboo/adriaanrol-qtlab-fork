0fda45ef11f7f5cb08254a9e21e06efb6eb06691
Adriaan <adriaan.rol@gmail.com>
2014-12-13 15:39:22 +0100

    added a setup.py file, going to test but expect directory conflicts

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='qtlab',
+    version='0.3.2',
+    description='Python based measurement environment',
+    url='https://github.com/AdriaanRol/test_pip_installer',
+    packages= find_packages(),
+    licence = 'GNU General Public License 2',
+)
